11b4190aa18267356df103c15a8abe1ed03ba39e
YDeltagon <git@ydeltagon.fr>
2025-02-09 16:08:35 +0100

    Performance Drops with Many Checkpoints #81

--- BeamJoyInterface/lua/ge/extensions/BJI/ui/WindowRace/RaceSolo.lua
+++ BeamJoyInterface/lua/ge/extensions/BJI/ui/WindowRace/RaceSolo.lua
@@ -143,8 +143,13 @@ local function drawSprint(ctxt)
             big = true,
         })
         :build()
+    local maxCheckpoints = 3  -- Nombre maximum de checkpoints à afficher
+    local totalCheckpoints = #lb.waypoints  -- Nombre total de checkpoints passés
+
+    local startIndex = math.max(1, totalCheckpoints - maxCheckpoints + 1)  -- Détermine où commencer
+
     local cols = ColumnsBuilder("BJIRaceSoloSprintLeaderboard", { labelWidth, -1 })
-    for iWp = 1, wpPerLap do
+    for iWp = startIndex, totalCheckpoints do
         if lb.waypoints[iWp] then
             local color = iWp == currentWp and TEXT_COLORS.HIGHLIGHT or TEXT_COLORS.DEFAULT
             cols:addRow({
